2b22ea3630b3eda4807d40bcd5c7e3a7c8d9ec00
adrianAlday <adrian.alday@gmail.com>
2026-05-17 20:41:14 -0400

    favicon, titles, colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import { Metadata } from "next";
 import Link from "next/link";
 
+export const metadata: Metadata = {
+  title: "Run Rankings",
+};
 const HomePage = async () => {
   return (
     <div>
       <main>
         <Link href={"/shoes"}>
-          <div className="text-sm font-semibold my-4">Shoes</div>
+          <div className="text-sm font-semibold my-4">👠 Shoes</div>
         </Link>
       </main>
     </div>
@@ -14,15 +18,8 @@ const HomePage = async () => {
 
 export default HomePage;
 
-// favicon
-// page titles
-// emojis in homepage link
 // make map of value variable name to db column name
 // display key name
 // explain math
-// bar color changes with value, apply by building rgb and passing to style
-// use vscode colors?
-// red yellow green blue purple
-// use comment color for lighter gray
 // fade out shsoes that are beaten on bounce price and weight
 // also show absorption for trail?
--- app/shoes/page.tsx
+++ app/shoes/page.tsx
@@ -2,86 +2,127 @@ import { createClient } from "@/utils/supabase/client";
 import { isDev } from "@/utils/isDev";
 import Refetch from "../_components/Refetch";
 import Link from "next/link";
+import { Metadata } from "next";
+
+type Shoe = { name: string; url: string } & { [key: string]: number };
+
+type ValueColors = [number, number[]][];
+
+export const metadata: Metadata = {
+  title: "Run Rankings - 👠 Shoes",
+};
 
 const ShoesPage = async () => {
   const supabase = createClient();
-  const { data, error } = await supabase.from("shoes").select("*");
+  const response = await supabase.from("shoes").select("*");
+  const { data, error } = response as { data: Shoe[]; error: null };
   if (error) {
     console.log(error);
   }
 
-  const chunkedData: { [key: string]: { [key: string]: string | number }[] } =
-    {};
-
   const sortKey = "cold_energy_returned_fore";
 
-  data
+  const values = data?.map((shoe) => shoe[sortKey]);
+  const maximumValue = Math.max(...values);
+  const minimumValue = Math.min(...values);
+
+  const minimumWidth = 5;
+
+  const dataByType = data
     ?.sort(
       (a, b) =>
-        ((b[sortKey] as number) || 0) - ((a[sortKey] as number) || 0) ||
-        (a.name as string).localeCompare(b.name as string),
+        (b[sortKey] || 0) - (a[sortKey] || 0) || a.name.localeCompare(b.name),
     )
-    .forEach((shoe) => {
-      if (!chunkedData[shoe.type]) {
-        chunkedData[shoe.type] = [];
-      }
-      chunkedData[shoe.type] = [...chunkedData[shoe.type], shoe];
-    });
+    .reduce(
+      (accumulator, shoe) => {
+        accumulator[shoe.type] = [...(accumulator[shoe.type] || []), shoe];
+
+        return accumulator;
+      },
+      {} as { [key: string]: Shoe[] },
+    );
+
+  const valueColors: ValueColors = [
+    [50, [239, 130, 119]], // red
+    [75, [249, 215, 73]], // yellow
+    [100, [73, 159, 248]], // blue
+  ];
+
+  const getValueRgb = (value: number, valueColors: ValueColors) => {
+    const firstCriteria = valueColors[0];
+    if (value <= firstCriteria[0]) {
+      return firstCriteria[1];
+    }
+
+    const lastCriteria = valueColors[valueColors.length - 1];
+    if (value >= lastCriteria[0]) {
+      return lastCriteria[1];
+    }
+
+    const higherIndex = valueColors.findIndex(
+      (criteria) => value <= criteria[0],
+    );
+    const higherCriteria = valueColors[higherIndex];
+    const lowerCriteria = valueColors[higherIndex - 1];
+    const split =
+      (value - lowerCriteria[0]) / (higherCriteria[0] - lowerCriteria[0]);
+
+    return lowerCriteria[1].map(
+      (lowerColor, index) =>
+        lowerColor + (higherCriteria[1][index] - lowerColor) * split,
+    );
+  };
+
+  const showRefetch = false;
 
   return (
     <div>
       <main>
-        {isDev && <Refetch />}
-
-        {Object.entries(chunkedData).map(([type, shoes]) => {
-          const maximumValue = Math.max(
-            ...shoes.map((shoe) => shoe[sortKey] as number),
-          );
-          const minimumValue = Math.min(
-            ...shoes.map((shoe) => shoe[sortKey] as number),
-          );
-          console.log("maximumValue", maximumValue);
-          console.log("minimumValue", minimumValue);
-
-          const minimumWidth = 5;
-
-          return (
-            <div key={type}>
-              <div className="my-4 text-[#9198a1]">
-                {type
-                  .toLowerCase()
-                  .replace(/\b\w/g, (char) => char.toUpperCase())}
-              </div>
-
-              {shoes.map((shoe) => {
-                const value = shoe[sortKey];
-                const width = value
-                  ? (100 * ((value as number) - minimumValue + minimumWidth)) /
-                    (maximumValue - minimumValue + minimumWidth)
-                  : minimumWidth;
-
-                return (
-                  <div key={shoe.name} className="my-4">
-                    <Link target="_blank" href={shoe.url as string}>
-                      <div className="text-sm font-semibold">{shoe.name}</div>
-
-                      <div
-                        className={`mt-2 border border-[#3d444d] rounded-md bg-[#238636] p-1 text-right text-xs font-semibold`}
-                        style={{ width: `${width}%` }}
-                      >
-                        {Math.round(shoe[sortKey] as number) || "-"}
-                      </div>
-                    </Link>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+        {isDev && showRefetch && <Refetch />}
 
         <Link href={"https://runrepeat.com"}>
-          <div className="my-4">Test data from RunRepeat</div>
+          <div className="my-8 text-sm font-semibold">
+            Test data from RunRepeat
+          </div>
         </Link>
+
+        {Object.entries(dataByType).map(([type, shoes]) => (
+          <div key={type}>
+            <div className="my-8 text-sm font-semibold">
+              {type
+                .toLowerCase()
+                .replace(/\b\w/g, (char) => char.toUpperCase())}
+            </div>
+
+            {shoes.map((shoe) => {
+              const value = shoe[sortKey];
+              const width = value
+                ? (100 * (value - minimumValue + minimumWidth)) /
+                  (maximumValue - minimumValue + minimumWidth)
+                : minimumWidth;
+
+              const backgroundColor = getValueRgb(value, valueColors);
+
+              return (
+                <div key={shoe.name} className="my-4">
+                  <Link target="_blank" href={shoe.url}>
+                    <div className="text-sm font-semibold">{shoe.name}</div>
+
+                    <div
+                      className={`border border-[rgb(42,43,44)] rounded-md p-1 text-right text-xs font-semibold text-[rgb(18,19,20)]`}
+                      style={{
+                        width: `${width}%`,
+                        backgroundColor: `rgb(${backgroundColor.join(",")})`,
+                      }}
+                    >
+                      {Math.round(shoe[sortKey]) || "-"}
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        ))}
       </main>
     </div>
   );
