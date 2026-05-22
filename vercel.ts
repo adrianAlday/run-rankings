923bceb69dcf7ec5bd7d15c6a0bc454a4b410539
adrianAlday <adrian.alday@gmail.com>
2026-05-22 14:07:58 -0400

    refactor 15 minute job

--- vercel.ts
+++ vercel.ts
@@ -1,6 +1,11 @@
 import { VercelConfig } from "@vercel/config/v1";
 
-const generateHourlyJobs = () => {};
+const everyHourArray = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23,
+];
+
+const everyFifteenMinuteArray = [0, 15, 30, 45];
 
 export const config: VercelConfig = {
   crons: [
@@ -32,23 +37,13 @@ export const config: VercelConfig = {
     //     path: "/api/shoes/refetch",
     //     schedule: `1 ${value} * * *`,
     //   })),
-    ...Array(24)
-      .keys()
-      .map((value) => ({
-        path: "/api/shoes/refetch",
-        schedule: `10 ${value} * * *`,
-      })),
 
     // every 15 minutes
-    // ...Array(24)
-    //   .keys()
-    //   .flatMap((value) =>
-    //     Array(4)
-    //       .keys()
-    //       .map((innerValue) => ({
-    //         path: "/api/shoes/refetch",
-    //         schedule: `${innerValue * 15 + 1} ${value} * * *`,
-    //       })),
-    //   ),
+    ...everyHourArray.flatMap((hour) =>
+      everyFifteenMinuteArray.map((minute) => ({
+        path: "/api/shoes/refetch",
+        schedule: `${minute * 15 + 1} ${hour} * * *`,
+      })),
+    ),
   ],
 };
