b547cbe0f3d918b11efe48646b60143579a06c95
adrianAlday <adrian.alday@gmail.com>
2026-05-22 14:01:42 -0400

    refetch shoes hourly

--- vercel.ts
+++ vercel.ts
@@ -4,40 +4,51 @@ const generateHourlyJobs = () => {};
 
 export const config: VercelConfig = {
   crons: [
+    // friends
+
     // {
     //   path: "/api/friends/refetch",
     //   schedule: "1 4 * * *",
     // },
 
+    // shoes
+
+    // every week
     // {
     //   path: "/api/shoes/refetch",
     //   schedule: "1 4 * * 6",
     // },
 
-    // daily
+    // every day
     // {
     //   path: "/api/shoes/refetch",
     //   schedule: "1 4 * * *",
     // },
 
-    // hourly
+    // every hour
     // ...Array(24)
     //   .keys()
     //   .map((value) => ({
     //     path: "/api/shoes/refetch",
     //     schedule: `1 ${value} * * *`,
     //   })),
-
-    // every 15 minutes
     ...Array(24)
       .keys()
-      .flatMap((value) =>
-        Array(4)
-          .keys()
-          .map((innerValue) => ({
-            path: "/api/shoes/refetch",
-            schedule: `${innerValue * 15 + 1} ${value} * * *`,
-          })),
-      ),
+      .map((value) => ({
+        path: "/api/shoes/refetch",
+        schedule: `10 ${value} * * *`,
+      })),
+
+    // every 15 minutes
+    // ...Array(24)
+    //   .keys()
+    //   .flatMap((value) =>
+    //     Array(4)
+    //       .keys()
+    //       .map((innerValue) => ({
+    //         path: "/api/shoes/refetch",
+    //         schedule: `${innerValue * 15 + 1} ${value} * * *`,
+    //       })),
+    //   ),
   ],
 };
