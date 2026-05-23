bc815e7c333bf2eb0633ba98509b3207ea5eafdf
adrianAlday <adrian.alday@gmail.com>
2026-05-23 19:29:54 -0400

    copy change

--- app/_components/Shoes.tsx
+++ app/_components/Shoes.tsx
@@ -123,7 +123,7 @@ const Shoes = ({ data }: ShoesProps) => {
         <div className="my-8 text-xs">
           <div className="my-4">
             <div className="my-1">
-              Relative rating of the energy{" "}
+              Relative ratings of the energy{" "}
               <span className="italic">absorbed</span> then{" "}
               <span className="italic">returned</span> in{" "}
               <span className="italic">cold</span> conditions with a penalty for{" "}
