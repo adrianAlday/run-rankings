63442cb30e351e9b98e96f25e0fc5764fbc73df2
adrianAlday <adrian.alday@gmail.com>
2026-05-23 22:35:31 -0400

    spinning loader

--- app/_components/LoadingWrapper.tsx
+++ app/_components/LoadingWrapper.tsx
@@ -7,10 +7,28 @@ type LoadingWrapperProps = {
 const LoadingWrapper = ({ children }: LoadingWrapperProps) => (
   <Suspense
     fallback={
-      <div className="flex items-center justify-center space-x-1.5">
-        <div className="rounded-full h-1.5 w-1.5 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.3s]" />
-        <div className="rounded-full h-1.5 w-1.5 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.15s]" />
-        <div className="rounded-full h-1.5 w-1.5 bg-[rgb(189,190,191)] animate-bounce" />
+      <div className="flex items-center justify-center">
+        <svg
+          className="mr-3 -ml-1 size-5 animate-spin text-[rgb(189,190,191)]"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-33"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          />
+
+          <path
+            className="opacity-100"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          />
+        </svg>
       </div>
     }
   >
