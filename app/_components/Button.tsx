b973a611f8d90d402fbfdb3336b01d76abbe179b
adrianAlday <adrian.alday@gmail.com>
2026-05-25 13:01:30 -0400

    home button size

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ url, text }: ButtonProps) => {
     <Link target="_blank" href={url}>
       <div
         className={
-          "my-8 border border-[rgb(52,53,54)] hover:border-[rgb(74,119,145)] rounded-md bg-[rgb(36,50,59)] py-1 flex items-center justify-center text-xs font-medium transition-all duration-700 transition-discrete"
+          "my-8 border border-[rgb(52,53,54)] hover:border-[rgb(74,119,145)] rounded-md bg-[rgb(36,50,59)] py-1 flex items-center justify-center text-sm font-medium transition-all duration-700 transition-discrete"
         }
       >
         <div>{text}</div>
