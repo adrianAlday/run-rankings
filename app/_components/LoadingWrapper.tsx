39f23eb26181ae215d252fb10fe189e7ea90cc25
adrianAlday <adrian.alday@gmail.com>
2026-05-25 15:31:25 -0400

    add comment

--- app/_components/LoadingWrapper.tsx
+++ app/_components/LoadingWrapper.tsx
@@ -32,14 +32,17 @@ const LoadingWrapper = ({ children }: LoadingWrapperProps) => (
           </svg>
         </div>
 
+        {
+          // more data needed to display on mobile
+        }
         <div className="text-[rgb(18,19,20)]">
-          There’s a wise saying that goes like this: A real gentleman never
-          discusses women he’s broken up with or how much tax he’s paid.
+          There{"'"}s a wise saying that goes like this: A real gentleman never
+          discusses women he{"'"}s broken up with or how much tax he{"'"}s paid.
           Actually, this is a total lie. I just made it up. Sorry! But if there
           really were such a saying, I think that one more condition for being a
           gentleman would be keeping quiet about what you do to stay healthy. A
-          gentleman shouldn’t go on and on about what he does to stay fit. At
-          least that’s how I see it.
+          gentleman shouldn{"'"}t go on and on about what he does to stay fit.
+          At least that{"'"}s how I see it.
         </div>
       </div>
     }
