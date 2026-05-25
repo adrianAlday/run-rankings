9d3331738c349efe73bb256c070fb45b146cfdce
adrianAlday <adrian.alday@gmail.com>
2026-05-25 11:40:44 -0400

    smaller button font

--- app/_components/Roster.tsx
+++ app/_components/Roster.tsx
@@ -248,7 +248,7 @@ const Roster = ({ data }: RosterProps) => {
 
                   changeParams({ range: rangeLabel });
                 }}
-                className={`mr-2 my-1 border border-[rgb(52,53,54)] hover:border-[rgb(74,119,145)] rounded-md ${range === rangeLabel ? "bg-[rgb(36,50,59)]" : "bg-[rgb(29,30,31)]"} py-1 px-2 shrink-0 flex items-center justify-center text-sm font-medium transition-all duration-700 transition-discrete`}
+                className={`mr-2 my-1 border border-[rgb(52,53,54)] hover:border-[rgb(74,119,145)] rounded-md ${range === rangeLabel ? "bg-[rgb(36,50,59)]" : "bg-[rgb(29,30,31)]"} py-1 px-2 shrink-0 flex items-center justify-center text-xs font-medium transition-all duration-700 transition-discrete`}
               >
                 {rangeLabel}
               </div>
