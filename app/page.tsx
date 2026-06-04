cd29a81d2f4599fc1d7848ffdf16408de5dfc971
adrianAlday <adrian.alday@gmail.com>
2026-06-04 15:44:32 -0400

    todos

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,7 @@ const HomePage = () => (
 
 export default HomePage;
 
-// friends refetch cron job go to 12 spaced every 2 hours?
 // make map of value variable name to db column name
-// display key name
 // also show absorption for trail?
 // add classnames library
-// show weight? sub bar? or energy bar, weight bar, overall bar?
-// how long are db fetches cached?
 // kudos page
