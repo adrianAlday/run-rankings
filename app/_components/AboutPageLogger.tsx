939157e3e5df66194435194d5f14ebfa9ff94444
adrianAlday <adrian.alday@gmail.com>
2026-06-04 09:27:56 -0400

    dont log about page on dev

--- app/_components/AboutPageLogger.tsx
+++ app/_components/AboutPageLogger.tsx
@@ -1,5 +1,6 @@
 import { headers } from "next/headers";
 import ClientLogger from "./ClientLogger";
+import { isDev } from "../_utils/isDev";
 
 const AboutPageLogger = async () => {
   const resolvedHeaders = await headers();
@@ -7,7 +8,7 @@ const AboutPageLogger = async () => {
 
   const message = `👋 Hey! Check out https://${host}/about`;
 
-  return <ClientLogger message={message} />;
+  return isDev ? null : <ClientLogger message={message} />;
 };
 
 export default AboutPageLogger;
