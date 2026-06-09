76527257cff825c4b3eabfcdee893d962b93ee34
adrianAlday <adrian.alday@gmail.com>
2026-06-09 14:53:40 -0400

    bounce loader

--- app/_components/LoadingWrapper.tsx
+++ app/_components/LoadingWrapper.tsx
@@ -8,28 +8,14 @@ const LoadingWrapper = ({ children }: LoadingWrapperProps) => (
   <Suspense
     fallback={
       <div>
-        <div className="flex items-center justify-center">
-          <svg
-            className="mr-3 -ml-1 size-5 animate-spin text-[rgb(189,190,191)]"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-33"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              stroke-width="4"
-            />
+        <div className="mt-8 flex justify-center animate-pulse">
+          <div className="flex space-x-2">
+            <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.3s]" />
 
-            <path
-              className="opacity-100"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
+            <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce [animation-delay:-0.15s]" />
+
+            <div className="rounded-full h-2 w-2 bg-[rgb(189,190,191)] animate-bounce" />
+          </div>
         </div>
 
         {
